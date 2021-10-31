f5a31ca3aa95e0c61a301b178d34d61c7379bcb1
Josh Marusek <jm553020@ohio.edu>
2021-10-30 20:28:22 -0400

    display menu and get choices

--- car-dealer.cc
+++ car-dealer.cc
@@ -10,17 +10,49 @@
 #include <cstdlib>
 using namespace std;
 
-///function prototypes
+/**
+ *  Function: getInteger
+ *  @brief Attempts to read integers from cin until a positive integer is entered.
+ * 
+ *  @return a positive integer from cin
+ */
+int getInteger();
+
 
 int main(int argc, char const *argv[]) {
+    int choice;
+
     // read options file
 
     // load options
 
-    // display selected options
+    while (true) {
+        // display menu and get choice
+        cout << "1. Select a model (E, L, X)" << endl;
+        cout << "2. Display available options and prices" << endl;
+        cout << "3. Add an option" << endl;
+        cout << "4. Remove an option" << endl;
+        cout << "5. Cancel order" << endl;
+        cout << "6. Quit" << endl;
+        cout << "Enter a choice: ";
+        choice = getInteger();
+        
+        // display selected options
 
-    // show menu
+        // show menu
 
-    // get choice
+        // get choice
+    }
+
     return 0;
 }/// main
+
+
+int getInteger() {
+    int result;
+    do {
+        cin >> result;
+    } while (result < 0);
+
+    return result;
+}
